288ecb24764f1ca0a7b53cad22cd4faed6fe9635
Marcos Macedo <marcosalvesmacedo@hotmail.com>
2021-05-02 22:37:20 -0300

    Create a new Post Method

--- src/main/java/br/com/macedo/real/controllers/HelloWorldController.java
+++ src/main/java/br/com/macedo/real/controllers/HelloWorldController.java
@@ -1,19 +1,24 @@
 package br.com.macedo.real.controllers;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping("/v1")
 public class HelloWorldController {
 
     @GetMapping("/hello-world")
-    public ResponseEntity<String> getHelloWorld(@RequestBody Object body) {
+    public ResponseEntity<String> getHelloWorld() {
+        log.info("Acessou o Hello World via Get");
         return new ResponseEntity<String>("Hello World", HttpStatus.OK);
     }
 
+    @PostMapping("/hello-world")
+    public ResponseEntity<String> postHelloWorld(@RequestBody Object body) {
+        log.info("Acessou o Hello World via Post");
+        return new ResponseEntity<String>("HelloWorld", HttpStatus.OK);
+    }
 }
